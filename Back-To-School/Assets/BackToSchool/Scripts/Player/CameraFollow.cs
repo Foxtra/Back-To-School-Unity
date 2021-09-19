f7d8babdfe1caa267c90f15ff825b9c597b214a1
Sergey Tkachev <sergey25121995@yandex.ru>
2021-09-19 19:57:33 +0300

    Added player following camera

---
+++ Back-To-School/Assets/BackToSchool/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+
+namespace Assets.BackToSchool.Scripts.Player
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        public Transform Target;
+        public Vector3 Offset;
+        public float SmoothSpeed = 0.125f;
+
+        private Vector3 _velocity = Vector3.zero;
+
+        private void FixedUpdate()
+        {
+            var desiredPosition = Target.position + Offset;
+            var smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref _velocity, SmoothSpeed);
+            transform.position = smoothedPosition;
+        }
+    }
+}

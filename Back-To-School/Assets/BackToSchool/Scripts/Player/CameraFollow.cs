ae51df9b67ba9f9bc6b67148fc6b80b1c121d238
Sergey Tkachev <sergey25121995@yandex.ru>
2021-09-22 22:04:05 +0300

    public fields changed to serialized private

--- Back-To-School/Assets/BackToSchool/Scripts/Player/CameraFollow.cs
+++ Back-To-School/Assets/BackToSchool/Scripts/Player/CameraFollow.cs
@@ -5,17 +5,15 @@ namespace Assets.BackToSchool.Scripts.Player
 {
     public class CameraFollow : MonoBehaviour
     {
-        public Transform Target;
-        public Vector3 Offset;
-        public float SmoothSpeed = 0.125f;
+        [SerializeField] private Transform _target;
+        [SerializeField] private Vector3 _offset;
+        [SerializeField] private float _smoothSpeed = 0.125f;
 
         private Vector3 _velocity = Vector3.zero;
 
         private void FixedUpdate()
         {
-            var desiredPosition = Target.position + Offset;
-            var smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref _velocity, SmoothSpeed);
-            transform.position = smoothedPosition;
+            transform.position = Vector3.SmoothDamp(transform.position, _target.position + _offset, ref _velocity, _smoothSpeed);
         }
     }
 }

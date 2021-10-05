366e153037288e04edb159a5b90f304a9cc53631
Sergey Tkachev <sergey25121995@yandex.ru>
2021-10-05 18:10:24 +0300

    Added UI for player

---
+++ Back-To-School/Assets/BackToSchool/Scripts/UI/PlayerAmmoInfo.cs
@@ -0,0 +1,37 @@
+﻿using Assets.BackToSchool.Scripts.Player;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Assets.BackToSchool.Scripts.UI
+{
+    public class PlayerAmmoInfo : MonoBehaviour
+    {
+        private Text _text;
+        private GameObject _player;
+        private PlayerShooting _playerShooting;
+
+        private void Awake()
+        {
+            _text = GetComponentInChildren<Text>();
+        }
+
+        private void Start()
+        {
+            _player = GameObject.FindGameObjectWithTag("Player");
+            _playerShooting = _player.GetComponent<PlayerShooting>();
+
+            _playerShooting.OnAmmoChanged += PlayerAmmoInfo_OnAmmoChanged;
+        }
+
+        private void PlayerAmmoInfo_OnAmmoChanged(object sender, PlayerAmmoArgs _args)
+        {
+            UpdateAmmoText(_args.NewAmmoValue, _args.MaxAmmoValue);
+        }
+
+        private void UpdateAmmoText(int newAmmoValue, int maxAmmoValue)
+        {
+            _text.text = $"{newAmmoValue} / {maxAmmoValue}";
+        }
+    }
+}

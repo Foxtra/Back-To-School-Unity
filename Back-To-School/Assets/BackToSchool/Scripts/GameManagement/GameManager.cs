6dc8e378410c46530c5a1b27fe1caca8bf3458bc
Sergey Tkachev <sergey25121995@yandex.ru>
2021-10-08 17:54:39 +0300

    add game over logic

---
+++ Back-To-School/Assets/BackToSchool/Scripts/GameManagement/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Assets.BackToSchool.Scripts.Player;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+namespace Assets.BackToSchool.Scripts.GameManagement
+{
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject _gameOverPanel;
+
+        private Button _restartButton;
+        private GameObject _player;
+        private PlayerInteracting _playerInteracting;
+
+        private float _gameOverDelay = 1f;
+
+        private void Start()
+        {
+            _restartButton = _gameOverPanel.GetComponentInChildren<Button>();
+            _restartButton.onClick.AddListener(RestartGame);
+
+            _player = GameObject.FindGameObjectWithTag("Player");
+            _playerInteracting = _player.GetComponent<PlayerInteracting>();
+            _playerInteracting.OnDeath += GameManager_OnPlayerDeath;
+        }
+
+        private void GameManager_OnPlayerDeath(object sender, EventArgs args)
+        {
+            Invoke(nameof(EndGame), _gameOverDelay);
+        }
+
+        private void EndGame()
+        {
+            _gameOverPanel.SetActive(true);
+        }
+
+        private void RestartGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}

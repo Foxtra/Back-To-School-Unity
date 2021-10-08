44f11394508c15335901c6cdb6c61d0b959bceed
Sergey Tkachev <sergey25121995@yandex.ru>
2021-10-08 19:20:42 +0300

    Add main menu

---
+++ Back-To-School/Assets/BackToSchool/Scripts/GameManagement/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using Assets.BackToSchool.Scripts.Constants;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+namespace Assets.BackToSchool.Scripts.GameManagement
+{
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField] private Button _startGameButton;
+        [SerializeField] private Button _exitGameButton;
+
+        private void Start()
+        {
+            _startGameButton.onClick.AddListener(StartGame);
+            _exitGameButton.onClick.AddListener(ExitGame);
+        }
+
+        private void ExitGame()
+        {
+            Application.Quit();
+        }
+
+        private void StartGame()
+        {
+            SceneManager.LoadScene(SceneNames.MainScene);
+        }
+    }
+}
